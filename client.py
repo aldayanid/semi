39c60492be2d44050fa97a2d2625dc5313465b54
Daniel Reznik <reznik@13-mbp.guest.corp.microsoft.com>
2021-09-05 21:25:30 +0300

    The client side is almost done, hopefully

--- client.py
+++ client.py
@@ -1,47 +1,46 @@
 import sys
+import socket
+
+BUFFER_SIZE = 1024
+HOST = '127.0.0.1'
+PORT = '8080'
+UTF8 = 'UTF-8'
 
 
 def select_file(selected_num: int) -> str:
 
-    while True:
+    while 1:
         if selected_num == 1:
-            # current_file = open("station_1.txt", "w", encoding='utf-8')
-            current_file = str("station_1.txt")
-            return current_file
+            return str("station_1.txt")
         elif selected_num == 2:
-            current_file = str("station_2.txt")
-            return current_file
+            return str("station_2.txt")
         elif selected_num == 3:
-            current_file = str("station_3.txt")
-            return current_file
+            return str("station_3.txt")
         else:
             print("Error! Please use only one of these numbers: 1, 2, 3")
-            continue #return to the beginning og the loop, again????? Continue doesn't work
+            break
+# How to get rid of the errors???
 
 
-def check_connection() -> bool:
-    return True
+def read_from_file(file_name: str) -> list:
+    with open(file_name, 'r', encoding=UTF8) as file:
+        return file.read().split("\n")
 
 
-def read_from_file(file_name: str) -> str:
-    line = ""
-    with open(file_name, 'r', encoding='UTF-8') as file:
-        while line in file.readline().rstrip():
-            print(line)
-
-
-def send_to_server(data_from_file: str, connection: bool) -> bool:
-    print("Everything is OK")
+def send_to_server(data_from_file: str) -> bool:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.send(data_from_file.encode(UTF8))
+        status = sock.recv(BUFFER_SIZE)
+        print(status, "has been received")
 
 
 def main():
     selected_num = int(input("Please select the station number,\njust input the number of station:\n 1, 2 or 3:\n"))
-    selected_file = select_file(selected_num)
-    print(selected_file)
-    data_from_file = read_from_file(selected_file)
+    selected_file_name = select_file(selected_num)
+    data_from_file = str(read_from_file(selected_file_name))
     print(data_from_file)
-    check_connection()
-    send_to_server(data_from_file, connection=True)
+    send_to_server(data_from_file)
 
 
 if __name__ == '__main__':
