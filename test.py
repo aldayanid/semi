2d837bc93a19ec9e3072c62f448cc31f837c46ae
aldayanid <aldayanid@gmail.com>
2021-09-16 15:28:55 +0300

    The finnal project of Python is ready

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
 import sqlite3
 from sqlite3 import Error, Connection
 
-conn = sqlite3.connect('stations.db')
-cursor = conn.execute("SELECT * FROM stations")
-result = cursor.fetchone()
-print("Operation done successfully", result)
+DB_SELECT_REQUEST = """
+    SELECT * FROM stations
+    ORDER BY time_st
+    DESC LIMIT 1"""
 
-conn.close()
+db_connection = None
+try:
+    db_connection = sqlite3.connect("stations.db")
+    db_connection.execute("""
+    CREATE TABLE IF NOT EXISTS stations (
+    st_num INTEGER,
+    time_st TIMESTAMP, 
+    alarm_1 INTEGER,
+    alarm_2 INTEGER    
+    );""")
+    print("Connection to SQLite DB successful")
+except Error as err:
+    print("The error", err, "occurred")
+
+curs = db_connection.execute(DB_SELECT_REQUEST)
+last_row_from_db = curs.fetchone()
+
+print("The last row is: ", last_row_from_db)
+last_row_str = " ".join(map(str, last_row_from_db))
+print("In string: ", last_row_str)
