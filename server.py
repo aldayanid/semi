e64da793d76bad13d569d93eb1e6a61bcfe925a8
aldayanid <aldayanid@gmail.com>
2021-09-16 15:36:06 +0300

    The project has been done

--- server.py
+++ server.py
@@ -1,94 +1,91 @@
-import os.path
-import socket
-import re
-from datetime import datetime
 import sqlite3
-from sqlite3 import Connection
+from sqlite3 import Error, Connection
 from typing import Optional
+from datetime import datetime
+import socket
+import json
 
 BUFFER_SIZE = 1024
 HOST = '127.0.0.1'
-PORT = 62434
+PORT = 8080
 UTF8 = 'UTF-8'
-PATH_DB = "stations.db"
+PATH_DB = 'stations.db'
 
-DB_CREATE_TABLE = """
-CREATE TABLE IF NOT EXISTS stations (
-    id INT AUTOINCREMENT PRIMARY KEY,
-    time TIMESTAMP,
-    num INT,
-    flag_1 INT,
-    flag_2 INT    
-);
-"""
+DB_CREATE_TABLE = '''
+    CREATE TABLE IF NOT EXISTS stations (
+    st_num INTEGER,
+    time_st TIMESTAMP, 
+    alarm_1 INTEGER,
+    alarm_2 INTEGER    
+    );'''
+
+DB_INSERT_ROW = '''
+    INSERT INTO stations VALUES
+    (?, ?, ?, ?
+    );'''
+
+DB_LAST_ROW = '''
+    SELECT * FROM stations
+    ORDER BY time_st
+    DESC LIMIT 1'''
 
 
-def insert_to_db(station_status: str, db_connection: Optional[Connection]) -> str:
-    conn = sqlite3.connect(PATH_DB)
-    curs = conn.db_connection()
+def insert_to_db(station_status: tuple, db_connection: Optional[Connection]) -> str:
+    curs = db_connection.cursor()
 
     timestamp = datetime.now()
-    time_st = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-
-    pattern = re.compile(r";|, ")
-    st_num, st_num, flag_1, flag_2 = pattern.split(station_status)
-
-    result = curs.execute("""INSERT INTO stations (time, st_num, flag_1, flag_2)
-                            VALUES (?, ?, ?, ?)""",
-                          (time_st, st_num, flag_1, flag_2)
-                          )
+    time_st = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
+    st_num, alarm_1, alarm_2 = station_status
+    station_status = curs.execute(DB_INSERT_ROW, (st_num, time_st, alarm_1, alarm_2))
     db_connection.commit()
-    return result
+    return station_status
 
 
-def create_db_connection(path: str) -> Optional[Connection]:
-    if os.path.exists(PATH_DB):
-        print("Stations database file does exist")
-        db_connection = sqlite3.connect(path)
-        print("Connection to SQLite DB is successful")
-        return db_connection
-    else:
-        print("DB doesn't exist, but we gonna fix it")
-        db_connection = sqlite3.connect(path)
-        curs = db_connection.cursor()
-        curs.execute(DB_CREATE_TABLE)
-        db_connection.commit()
-        return db_connection
+def read_from_db(db_connection: Optional[Connection]) -> str:
+    cursor = db_connection.execute(DB_LAST_ROW)
+    last_row_from_db = cursor.fetchone()
+    print("The last row is: ", last_row_from_db)
+    last_row_str = " ".join(map(str, last_row_from_db))
+    return  last_row_str
 
 
-def handle_request(sock: socket.socket, bytes_array: bytes, db_connection: Connection) -> None:
-    station_status = str(bytes_array.decode(UTF8))
-    status = insert_to_db(station_status, db_connection)
-    sock.send(status.encode(UTF8))
+def convert_from_raw(bytes_array: bytes) -> tuple:
+    received_array = bytes_array.decode(UTF8)
+    json_current_status = json.loads(received_array)
+    return json_current_status
 
-def convert_from_raw(bytes_array: bytes) -> str:
-    decoded_array = str(bytes_array.decode(UTF8))
-    print(decoded_array)
-    return decoded_array
+
+def create_db_connection(path_db: str) -> Optional[Connection]:
+    db_connection = None
+    try:
+        db_connection = sqlite3.connect(path_db)
+        db_connection.execute(DB_CREATE_TABLE)
+        print("Connection to SQLite DB successful")
+    except Error as err:
+        print("The error", err, "occurred")
+    return db_connection
+
+
+def handle_request(sock: socket.socket, db_connection: Connection) -> None:
+    station_status_raw = sock.recv(BUFFER_SIZE)
+    station_status = convert_from_raw(station_status_raw)
+    insert_to_db(station_status, db_connection)
+    station_status_str = read_from_db(db_connection)
+    print("Feedback: ", station_status_str)
+    sock.send(station_status_str.encode(UTF8))
+
 
 def main():
-    # while True:
-    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
-    #         server_sock.bind((HOST, PORT))
-    #         print("Starting up on IP:", HOST, "port:", PORT)
-    #         server_sock.listen(5)
-    #         while True:
-    #             print("Waiting for a connection")
-    #             connection, addr = server_sock.accept()
-    #             try:
-    #                 data_from_client = connection.recv(BUFFER_SIZE)
-    #                 print("received: ", data_from_client)
-    #                 db_connection = create_db_connection(PATH_DB)
-    #                 handle_request(server_sock, db_connection, data_from_client)
-    #             #except??
-    #             finally:
-    #                 pass
-    db_connection = create_db_connection(PATH_DB)
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.bind((HOST, PORT))
-        sock.listen(5)
-        connection, addr = sock.accept()
-        handle_request(connection, db_connection)
+        while True:
+            db_connection = create_db_connection(PATH_DB)
+            try:
+                server = socket.create_server((HOST, PORT))
+                sock, addr = server.accept()
+                server.close()
+                handle_request(sock, db_connection)
+            except socket.error as err:
+                print("Error while connecting: ", err)
+                quit()
 
 
 if __name__ == '__main__':
